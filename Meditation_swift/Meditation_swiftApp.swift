09a2cefb7bc063c33f547d769f00cc4780f1a90b
Raeyoung Kim <kimrae@sheridancollege.ca>
2024-03-03 22:18:18 -0500

    Initial Commit

---
+++ Meditation_swift/Meditation_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meditation_swiftApp.swift
+//  Meditation_swift
+//
+//  Created by Raeyoung Kim on 2024-03-03.
+//
+
+import SwiftUI
+
+@main
+struct Meditation_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
